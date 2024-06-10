dd691873dbb834c8cdebfdb146f700ce99e7c56b
ejisemm <emmanuel.jiss@ericsson.com>
2024-06-10 09:42:55 +0100

    Fixed. Finally

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, url_for, render_template
-app = Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='website/templates', static_folder='website/static')
 
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+
+@app.route('/booking')
+def booking():
+    return render_template('booking.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
