1b1aa4f78057871aae5d6719a2a98e6826c7fb3b
ejisemm <emmanuel.jiss@ericsson.com>
2024-06-11 09:17:00 +0100

    Typo

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, template_folder='/website/template', static_folder='/website/static')
+app = Flask(__name__, template_folder='website/template', static_folder='website/static')
+
+USERNAME = 'root'
+PASSWORD = ''
+HOST = 'localhost'
+DB_NAME = 'booking'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + USERNAME + ':' + PASSWORD + '@' + HOST + '/' + DB_NAME
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    __tablename__ = 'boooking'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
 
 @app.route('/')
 def index():
@@ -25,23 +42,6 @@ def about():
 @app.route('/account')
 def account():
     return render_template('account.html')
-
-
-USERNAME = 'root'
-PASSWORD = ''
-HOST = 'localhost'
-DB_NAME = 'booking'
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + USERNAME + ':' + PASSWORD + '@' + HOST + '/' + DB_NAME
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-
-class Item(db.Model):
-    __tablename__ = 'boooking'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
 
 
 if __name__ == '__main__':
