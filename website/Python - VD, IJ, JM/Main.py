7b985af0f742eaabd52a2f68b69ac94b347b65e3
Vidhur <vidhur.dandu@ericsson.com>
2024-06-11 09:22:12 +0100

    More python and have now added functional submit botton (i think)

--- website/Python - VD, IJ, JM/Main.py
+++ website/Python - VD, IJ, JM/Main.py
@@ -1,3 +1,18 @@
 Booking = []
 
+def submit_booking(email, pitch, start, end, amenities):
+    Booking.append(email)
+    Booking.append(pitch)
+    Booking.append(start)
+    Booking.append(end)
+    Booking.append(amenities)
+
+Account = []
+
+def submit_account(username, email, password1,password2):
+    Account.append(username)
+    Account.append(email)
+    if password1 == password2:
+        Account.append(password1)
+
 
