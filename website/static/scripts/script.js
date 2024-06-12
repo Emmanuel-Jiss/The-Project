0f916b0afb6025648505d786e1cdb09663503267
ejisemm <emmanuel.jiss@ericsson.com>
2024-06-12 20:31:52 +0100

    Fixed Carousel

--- website/static/scripts/script.js
+++ website/static/scripts/script.js
@@ -24,9 +24,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
-//- Using a function pointer:
-document.getElementById("clickMe").onclick = Confirm;
-function doConfirm {
-    document.getElementById("snow").style = "display: show;";
-}
