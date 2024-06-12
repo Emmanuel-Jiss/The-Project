11e803737801ca39ff41d51c056a5bff4e4f908e
Vidhur <vidhur.dandu@ericsson.com>
2024-06-12 16:48:27 +0100

    Nav bar is consiis

--- app.py
+++ app.py
@@ -26,10 +26,6 @@ def about():
 def account():
     return render_template('account.html')
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
