8007334b946e469c307c70666e41aea4171127f4
ejisemm <emmanuel.jiss@ericsson.com>
2024-06-12 11:38:16 +0100

    More work on the navbar

--- app.py
+++ app.py
@@ -26,6 +26,9 @@ def about():
 def account():
     return render_template('account.html')
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 
 if __name__ == '__main__':
